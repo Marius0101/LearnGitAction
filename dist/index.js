83fca44b905c2ffb213be8f58f760543987103f7
Marius Mihalcea <mariusoctavian010@gmail.com>
2023-11-06 14:22:52 +0200

    small change

--- dist/index.js
+++ dist/index.js
@@ -2758,18 +2758,11 @@ const wait_1 = __nccwpck_require__(259);
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
+
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        const waitFunction = wait_1.wait;
-        await waitFunction(isNaN(ms)? parseInt(ms, 10): 10);
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        const { run } = require('./main');
+        run();
     }
     catch (error) {
         // Fail the workflow run if an error occurs
