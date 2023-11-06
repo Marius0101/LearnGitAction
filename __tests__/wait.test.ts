fd164f2f8d961f0ad5237fd3efd9bed6c8dc3db1
Marius Mihalcea <mariusoctavian010@gmail.com>
2023-11-06 13:16:33 +0200

    remove test

--- __tests__/wait.test.ts
+++ __tests__/wait.test.ts
@@ -6,11 +6,6 @@ import { wait } from '../src/wait'
 import { expect } from '@jest/globals'
 
 describe('wait.ts', () => {
-  it('throws an invalid number', async () => {
-    const input = parseInt('foo', 10)
-    expect(isNaN(input),).toBe(true, )
-    await expect(wait(input)).rejects.toThrow('milliseconds not a number ....')
-  })
 
   it('waits with a valid number', async () => {
     const start = new Date()
