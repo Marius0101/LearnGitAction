691f2d6152b2c3bfff3584c7a1d6fe62ef4ccb00
Marius Mihalcea <mariusoctavian010@gmail.com>
2023-11-06 12:50:56 +0200

    change msg

--- src/wait.ts
+++ src/wait.ts
@@ -3,10 +3,10 @@
  * @param milliseconds The number of milliseconds to wait.
  * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
  */
-export async function wait(milliseconds=1000): Promise<string> {
+export async function wait(milliseconds:number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      throw new Error('milliseconds not a number :(')
     }
 
     setTimeout(() => resolve('done!'), milliseconds)
